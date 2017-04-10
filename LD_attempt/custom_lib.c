97e0a85a6a97f1751020e3bfbe0a04dfff4cfbb9
Akshay Nagpal <akshaynagpal@github.com>
2017-04-09 22:00:22 -0400

    printf working now

--- LD_attempt/custom_lib.c
+++ LD_attempt/custom_lib.c
@@ -12,16 +12,5 @@ int printf(const char *format, ...){
 	// evil code end
 	orig_printf_type orig_printf;
 	orig_printf = (orig_printf_type)dlsym(RTLD_NEXT,"printf");
-	orig_printf("ERROR inside!");
-	return orig_printf("ERROR!");
-	
-	// UNUSED CODE
-	//va_list arg;
-	//int done;
-	//va_start(arg,format);
-	//char *errormsg = "ERROR inside custom_libc.c";
-	//done = vfprintf(stdout,errormsg,arg);
-	//done = vfprintf(stdout,format,arg);
-	//va_end (arg);
-	//return done;	
+	return orig_printf("ERROR!");	
 }
--- LD_attempt/helloworld.c
+++ LD_attempt/helloworld.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
 int main(){
-	printf("hello world!\n");
+	printf("hello world!%d\n",1);
 	return 1;
 }
