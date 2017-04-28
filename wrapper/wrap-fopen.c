ec1cece929d9285d26084b5458e997d3e5be01aa
Kunal Baweja <bawejakunal15@gmail.com>
2017-04-28 11:16:56 -0400

    fopen fail

--- wrapper/wrap-fopen.c
+++ wrapper/wrap-fopen.c
@@ -6,14 +6,28 @@
 #define _GNU_SOURCE
 #include <dlfcn.h>
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
 
-//
+// struct to maintain counts of calls
+typedef struct
+{
+    long fopen;
+    long printf;
+} call_counts;
+
+// global counts of each call
+static call_counts global_counts;
+
+// typedef for fopen
 typedef FILE* (*orig_fopen_t)(const char *path, const char *mode);
 
 FILE *fopen(const char *path, const char *mode){
-    fprintf(stdout, "WRAPPER FOPEN CALLED\n");
+    ++global_counts.fopen;
+    char* fail_num = getenv("FOPEN_FAIL");
+    if (fail_num != NULL && atol(fail_num) == global_counts.fopen)
+        return NULL;
     orig_fopen_t orig_fopen;
     orig_fopen = (orig_fopen_t)dlsym(RTLD_NEXT, "fopen");
     return orig_fopen(path, mode);
-    // return NULL;
 }
