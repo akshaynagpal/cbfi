ff95a5e0763f1ba26935e66ed988697af447ec28
akshaynagpal <akshay2626@gmail.com>
2017-04-28 13:05:48 -0400

    added env variable check to calloc and malloc

--- wrapper/wrap-calloc.c
+++ wrapper/wrap-calloc.c
@@ -3,11 +3,21 @@
 #include <dlfcn.h>
 #include <stdlib.h>
 
+// struct to maintain counts of calls
+typedef struct
+{
+    long calloc;
+} call_counts;
+
+// global counts of each call
+static call_counts global_counts;
+
 typedef void* (*orig_calloc_type)(size_t nelem,size_t elsize);
 
 void *calloc(size_t nelem,size_t elsize){
-	// error calloc
-	return NULL;
+	char* fail_num = getenv("CALLOC_FAIL");
+    if (fail_num != NULL && atol(fail_num) == global_counts.calloc)
+        return NULL;
 
 	// normal calloc
 	orig_calloc_type orig_calloc;
--- wrapper/wrap-malloc.c
+++ wrapper/wrap-malloc.c
@@ -3,10 +3,23 @@
 #include <dlfcn.h>
 #include <stdlib.h>
 
+// struct to maintain counts of calls
+typedef struct
+{
+    long malloc;
+} call_counts;
+
+// global counts of each call
+static call_counts global_counts;
+
 typedef void* (*orig_malloc_type)(size_t size);
 
 void *malloc(size_t size){
-	// return NULL;
+	char* fail_num = getenv("MALLOC_FAIL");
+    if (fail_num != NULL && atol(fail_num) == global_counts.malloc)
+        return NULL;
+
+    // normal malloc
 	orig_malloc_type orig_malloc;
 	orig_malloc = (orig_malloc_type)dlsym(RTLD_NEXT,"malloc");
 	return orig_malloc(size);
