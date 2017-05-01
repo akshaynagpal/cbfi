5f419b2cae17eb5c01b5b4fdc37dac292ae39e8b
Kunal Baweja <bawejakunal15@gmail.com>
2017-04-30 21:25:55 -0400

    update gitignore to include subdirectories

---
+++ wrapper/wrapper.h
@@ -0,0 +1,32 @@
+/**
+ *
+ * Wrapper linked library header file
+ *
+ **/
+
+#define _GNU_SOURCE
+
+#include <dlfcn.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <stdlib.h>
+
+// struct to maintain counts of calls
+typedef struct
+{
+    unsigned long fopen;
+    unsigned long printf;
+    unsigned long malloc;
+    unsigned long calloc;
+} call_counts;
+
+/**
+ * global variable to maintain counts
+ *
+ *  declare static call_counts global_counts in exactly one .c file
+ *
+ * Declaration in wrap-fopen.c
+ */
+extern call_counts global_counts;
