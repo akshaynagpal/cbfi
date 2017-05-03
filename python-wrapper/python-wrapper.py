bbd328c56632c52b28f00aff1d30f3b265385dee
Siddharth Shah <sas2387@columbia.edu>
2017-05-03 01:38:01 -0400

    Changes in python wrapper

--- python-wrapper/python-wrapper.py
+++ python-wrapper/python-wrapper.py
@@ -3,6 +3,7 @@ from pprint import pprint
 import sys
 import json
 import subprocess
+from collections import deque
 
 """
 Using pygdbmi.
@@ -41,12 +42,95 @@ def get_linecount_string(out, source_file_name, abrupt):
 	return exec_count
 
 def should_not_proceed(allexecutions, exec_string):
-	last2_execs = allexecutions[-2:]
+	"""last2_execs = allexecutions[-2:]
 
 	if len(last2_execs) == 2 and exec_string == last2_execs[0] and exec_string == last2_execs[1]:
 		return True
 	else:
+		return False"""
+	last_exec = allexecutions[-1:]
+
+	if len(last_exec) == 1 and exec_string == last_exec[0]:
+		return True
+	else:
 		return False
+
+def do_processing(executable, arguments, source_file_name, source_file_path, call_to_fail, call_nums_to_fail):
+
+	exec_args = arguments.split(" ")
+
+	gdb_exec_args = ['-nx','--quiet', '-interpreter=mi2', '--args', executable] + exec_args
+
+	gdbmi = GdbController(gdb_args=gdb_exec_args)
+
+	responses = gdb_read(gdb_controller=gdbmi)
+
+	gdbmi.write('set environment LD_PRELOAD=../wrapper/wrap-fopen.so', read_response=False)
+
+	
+	#We will be setting the environment variables for failing libc calls over here
+
+	call_nums = ','.join(map(str,call_nums_to_fail))
+	gdbmi.write('set environment {0}={1}'.format(call_to_fail, call_nums), read_response=False)
+
+	gdbmi.write('br main', read_response=False)
+	responses = gdb_read(gdb_controller=gdbmi)
+	#pprint(responses)
+	#print("\n\n")
+
+	gdbmi.write('run', read_response=False)
+	responses = gdb_read(gdb_controller=gdbmi)
+	#pprint(responses)
+	#print("\n\n")
+
+	# The program execution stops here because break on main would have reached
+	break_outer = False
+	abrupt =False
+	while(True):
+
+		gdbmi.write('call __gcov_flush()', read_response=False)
+
+		gdbmi.write('next', read_response=False)
+		responses = gdb_read(gdb_controller=gdbmi)
+
+		#pprint(responses)
+		#print("\n\n")
+
+		for response in responses:
+			if response['message'] == 'stopped' \
+				and response['payload']['reason'] == 'end-stepping-range' \
+				and response['payload']['frame']['file'] == source_file_name:
+				current_lineno = int(response['payload']['frame']['line'])
+
+			elif response['message'] == 'stopped' and response['payload']['reason'] == 'exited-signalled':
+				break_outer = True
+				abrupt = True
+			elif response['message'] =='stopped' and response['payload']['reason'] == 'exited-normally':
+				break_outer = True
+
+		
+		if break_outer:
+			gdbmi.write('call __gcov_flush()', read_response=False)
+			break
+
+	gdbmi.exit()
+
+	process = subprocess.Popen(['gcov', '-i', source_file_path], stdout=subprocess.PIPE)
+	out, err = process.communicate()
+	
+	with open('{0}.{1}'.format(source_file_name,'gcov'), 'r') as gcovfile:
+		gcovdata = gcovfile.read().split('\n')
+
+	#pprint(gcovdata)
+	exec_string = get_linecount_string(gcovdata, source_file_name, abrupt)
+
+	# Delete gcda file
+	subprocess.call(['rm', '{0}.{1}'.format(source_file_path[:-2],'gcda')])
+
+	# Delete gcov file
+	subprocess.call(['rm', '{0}.{1}'.format(source_file_name,'gcov')])
+
+	return (exec_string, abrupt)
 
 libc_mapping = {
 	'FOPEN':'FOPEN_FAIL',
@@ -70,6 +154,7 @@ if __name__ == "__main__":
 	if 'FAIL_CALL' not in config_json \
 		or 'SOURCE_FILE_PATH' not in config_json \
 		or 'SOURCE_FILE_NAME' not in config_json \
+		or 'ARGUMENTS' not in config_json \
 		or 'EXECUTABLE' not in config_json :
 		print "Incorrect format: Refer to sampleconfig.json for correct format"
 		exit()
@@ -80,7 +165,8 @@ if __name__ == "__main__":
 	source_file_path = config_json['SOURCE_FILE_PATH']
 	source_file_name = config_json['SOURCE_FILE_NAME']
 	executable = config_json['EXECUTABLE']
-	call_num_to_fail = -1
+	arguments = config_json['ARGUMENTS']
+	call_num_to_fail = 0
 
 	current_run = 1
 
@@ -88,88 +174,53 @@ if __name__ == "__main__":
 
 	error_lines = []
 
+	queue = deque()
+
 	while True:
 
-		gdbmi = GdbController(gdb_args=['--args '+executable,'-nx','--quiet', '-interpreter=mi2'])
+		call_num_to_fail += 1
 
-		responses = gdb_read(gdb_controller=gdbmi)
-
-		gdbmi.write('set environment LD_PRELOAD=../wrapper/wraplib.so', read_response=False)
-
-		"""
-		We will be setting the environment variables for failing libc calls over here
-		"""
-		gdbmi.write('set environment {0}={1}'.format(call_to_fail, call_num_to_fail), read_response=False)
-
-		gdbmi.write('br main', read_response=False)
-		responses = gdb_read(gdb_controller=gdbmi)
-		#pprint(responses)
-		#print("\n\n")
-
-		gdbmi.write('run', read_response=False)
-		responses = gdb_read(gdb_controller=gdbmi)
-		#pprint(responses)
-		#print("\n\n")
-
-		# The program execution stops here because break on main would have reached
-		break_outer = False
-		abrupt =False
-		while(True):
-
-			gdbmi.write('call __gcov_flush()', read_response=False)
-
-			gdbmi.write('next', read_response=False)
-			responses = gdb_read(gdb_controller=gdbmi)
-
-			#pprint(responses)
-			#print("\n\n")
-
-			for response in responses:
-				if response['message'] == 'stopped' \
-					and response['payload']['reason'] == 'end-stepping-range' \
-					and response['payload']['frame']['file'] == source_file_name:
-					current_lineno = int(response['payload']['frame']['line'])
-
-				elif response['message'] == 'stopped' and response['payload']['reason'] == 'exited-signalled':
-					break_outer = True
-					abrupt = True
-				elif response['message'] =='stopped' and response['payload']['reason'] == 'exited-normally':
-					break_outer = True
-
-			
-			if break_outer:
-				gdbmi.write('call __gcov_flush()', read_response=False)
-				break
-
-		gdbmi.exit()
-
-		process = subprocess.Popen(['gcov', '-i', source_file_path], stdout=subprocess.PIPE)
-		out, err = process.communicate()
-
-		#print out
-		
-		with open('{0}.{1}'.format(source_file_name,'gcov'), 'r') as gcovfile:
-			gcovdata = gcovfile.read().split('\n')
-
-		#pprint(gcovdata)
-		exec_string = get_linecount_string(gcovdata, source_file_name, abrupt)
-
-		# Delete gcda file
-		subprocess.call(['rm', '{0}.{1}'.format(source_file_path[:-2],'gcda')])
-
-		# Delete gcov file
-		subprocess.call(['rm', '{0}.{1}'.format(source_file_name,'gcov')])
+		exec_string, abrupt = do_processing(executable, arguments, source_file_name, source_file_path, call_to_fail, [call_num_to_fail])
 
 		print exec_string
-		
+	
 		if abrupt:
-			error_lines.append(current_lineno)
+			#error_lines.append(current_lineno)
+			pass
 
 		if should_not_proceed(all_executions, exec_string):
 			break
+		elif not abrupt:
+			lines = len(exec_string)
+			zeroes = exec_string.count('0')
+			ones = exec_string.count('1')
+			coverage = (ones - zeroes) / lines
+
+			queue.append(
+				{
+					'call_fail':call_to_fail,
+					'call_num':[call_num_to_fail],
+					'coverage':coverage
+				}
+			)
 
 		all_executions.append(exec_string)
 
 		current_run += 1
 
-	print error_lines
+		
+
+	print "Queue:",queue
+	print "Check lines:", error_lines
+	"""
+	while len(queue) != 0:
+		queue_item = queue.pop()
+
+		current_call_fail = queue_item['call_fail']
+		current_call_fail_num = queue_item['call_num']
+		max_call = current_call_fail[-1:]
+
+		#while True:
+
+	print "Queue:",queue
+	print "Check lines:", error_lines"""
