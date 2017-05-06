d43abd5e1a4e90c8a32c884c6681ba222462a7b0
Siddharth Shah <sas2387@columbia.edu>
2017-05-05 22:26:20 -0400

    Code for checking address

--- backtrace_expt/check_back.c
+++ backtrace_expt/check_back.c
@@ -1,20 +1,33 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <execinfo.h>
 #include <stdlib.h>
-
+#include <dlfcn.h>
 
 void handler(char *caller) {
   void *array[10];
   size_t size;
   printf("Stack Trace Start for %s\n",caller);
   size = backtrace(array, 10);
-  backtrace_symbols_fd(array, size, 2);
+  Dl_info info[size];
+  for(int i=0;i <size; i++) {
+	  dladdr(array[i],&info[i]);
+	  if(&info[i] != NULL) {
+	  	printf("Got it %s\n",info[i].dli_fname);
+	  	printf("Got it %s\n",info[i].dli_sname);
+	  }
+	}
+  backtrace_symbols(array, size);
   printf("Stack Trace End\n");
 }
 
-void car() {
-    handler("car()");
+void gcov_flush() {
+	handler("car()");
     printf("Continue Execution");
+}
+
+void car() {
+    gcov_flush();
 }
 void baz() {car(); }
 void bar() { baz(); }
