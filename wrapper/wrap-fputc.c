1293e8450ea8fae19ce718f50bd2224417604677
akshaynagpal <akshay2626@gmail.com>
2017-05-07 02:37:27 -0400

    putc updated

--- wrapper/wrap-fputc.c
+++ wrapper/wrap-fputc.c
@@ -10,22 +10,27 @@ typedef int (*orig_fputc_type)(int c, FILE *stream);
 
 int fputc(int c, FILE *stream){
     char* fail_nums = getenv("FPUTC_FAIL");
-    if (fail_nums != NULL){
+    unsigned int *numbers = NULL; // filled by parse function
+    int original = btrace_has_gcov();   // check if called by gcov
+    unsigned int length, i;
+    int error = 0;
+
+    if (fail_num != NULL && original == 0) {
         ++global_counts.fputc;
-        // Note: strtok requires the a char* or char[] 
-        // that it can modify and const char* won't work
-        char *num; 
-        num = strtok(fail_nums,",");
-        while(num != NULL) {
-            if (atol(num) == global_counts.fputc) {
-                errno = EACCES;
-                return EOF;
+        length = parse_fail_str(&numbers, fail_num);
+        // iterate over numbers array
+        for (i = 0; i < length; i++) {
+            if (numbers[i] == global_counts.fputc) {
+                error = -1;
+                break;
             }
-            num = strtok(NULL,",");
+        }
+        free(numbers); // release memory
+        if (error == -1) {
+            errno = EACCES;
+            return EOF;
         }
     }
-
-    
     orig_fputc_type orig_fputc;
     orig_fputc = (orig_fputc_type)dlsym(RTLD_NEXT, "fputc");
     return orig_fputc(c, stream);
